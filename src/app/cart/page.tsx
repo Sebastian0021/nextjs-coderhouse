078761ead99d1eae8ac3c840ed91b3f121400ddf
Sebastian0021 <sebastianalefuentespe@gmail.com>
2025-02-07 13:19:57 -0300

    Dandole funcionalidad al carrito

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,15 +6,6 @@ export const metadata = {
 };
 
 const Cart = () => {
-  // if (items.length === 0) {
-  //   return (
-  //     <div className="max-w-2xl mx-auto p-4">
-  //       <h1 className="text-3xl font-bold mb-4">Cart</h1>
-  //       <p className="text-lg">Your cart is empty.</p>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="max-w-2xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Cart</h1>
