76b0e58a778d5afcde9e579e24bcf281a1529d43
Sebastian0021 <sebastianalefuentespe@gmail.com>
2025-02-09 13:35:53 -0300

    agregando y cambiando logo de la página

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import "../../app/globals.css";
 import { useState } from "react";
 import { IoClose, IoMenu, IoCart } from "react-icons/io5";
+import logo from "../../../public/logo.png";
+import Image from "next/image";
 
 const NavLinks = ({ closeMenu }: { closeMenu: () => void }) => {
   return (
@@ -50,7 +52,7 @@ function Navbar() {
     <header className="bg-gray-600 py-4 px-2 sticky top-0">
       <nav className="container mx-auto flex justify-between">
         <Link href={"/"} className="text-white text-2xl font-bold">
-          Logo
+          <Image src={logo} alt="logo" width={50} height={50} />
         </Link>
 
         <div className="flex items-center gap-8">
